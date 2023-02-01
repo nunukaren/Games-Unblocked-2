1ae7255e3d65673cc541a770b3454e038b41be03
nunukaren <120516646+nunukaren@users.noreply.github.com>
2023-01-31 19:50:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const submit = document.querySelector('#submit');
 const searchbar = document.querySelector('#search');
 submit.onclick = () => {
-	if(search.value == 'Math69' || search.value == 'math69'){
+	if(search.value == 'Math420' || search.value == 'math420'){
 	location.href='https://sites.google.com/view/monkey17/home';
 	}
 }
